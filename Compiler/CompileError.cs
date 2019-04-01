c8690efb5ee6edd545923d85552cf1cda1697a5e
Sean Moss <moss.seank@gmail.com>
2019-04-01 14:40:28 -0600

    Initial compiler API to code  an executable against

---
+++ Compiler/CompileError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSLang
+{
+	/// <summary>
+	/// Contains information about an error that occurs in the compiler.
+	/// </summary>
+	public sealed class CompileError
+	{
+
+	}
+}
---
+++ Compiler/CompileOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSLang
+{
+	/// <summary>
+	/// Represents the set of options to use when compiling an .ssl file into SPIR-V bytecode.
+	/// </summary>
+	public sealed class CompileOptions
+	{
+
+	}
+}
